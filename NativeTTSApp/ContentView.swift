fbc08c81b8fa711e460b0ea409ee6d0a6db18d46
morik <kosuke.mori.5120@gmail.com>
2026-03-17 20:21:39 +0900

    Initial Commit

---
+++ NativeTTSApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NativeTTSApp
+//
+//  Created by Kosuke Mori on 2026/03/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NativeTTSApp/NativeTTSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NativeTTSAppApp.swift
+//  NativeTTSApp
+//
+//  Created by Kosuke Mori on 2026/03/17.
+//
+
+import SwiftUI
+
+@main
+struct NativeTTSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
